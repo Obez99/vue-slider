cff2516c959278aa258c25fdc9ceb4fb9c94de53
Obez99 <giuliogx@live.it>
2021-10-29 17:08:22 +0200

    Adds bonus folder, makes nextImage function work every 3 seconds

---
+++ bonus/js/scripts.js
@@ -0,0 +1,60 @@
+Vue.config.devtools = true;
+
+new Vue({
+  el: "#app",
+  data: {
+    elementsList: [
+      {
+        image: "img/01.jpg",
+        title: "Svezia",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis."
+      },
+
+      {
+        image: "img/02.jpg",
+        title: "Svizzera",
+        text: "Lorem ipsum."
+      },
+
+      {
+        image: "img/03.jpg",
+        title: "Gran Bretagna",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+      },
+
+      {
+        image: "img/04.jpg",
+        title: "Germania",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,"
+      },
+
+      {
+        image: "img/05.jpg",
+        title: "Paradise",
+        text: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,"
+      }
+    ],
+
+    currentImage: 0
+  },
+
+  methods: {
+    nextImage: function () {
+      this.currentImage++;
+
+      if (this.currentImage > this.elementsList.length - 1)
+        this.currentImage = 0;
+    },
+
+    prevImage: function () {
+      this.currentImage--
+
+      if (this.currentImage < 0)
+        this.currentImage = 4;
+    }
+  },
+
+  mounted() {
+    setInterval(this.nextImage, 3000)
+  }
+})
